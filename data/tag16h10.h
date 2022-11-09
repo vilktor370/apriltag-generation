0f1d70efc8f4b2d95024b26e029ffe16afc5f54f
Haochen <haochyu@umich.edu>
2022-11-08 20:25:42 -0500

    add data for 16h10

---
+++ data/tag16h10.h
@@ -0,0 +1,17 @@
+#ifndef _TAG16H10
+#define _TAG16H10
+
+#include "apriltag.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+apriltag_family_t *tag16h10_create();
+void tag16h10_destroy(apriltag_family_t *tf);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
